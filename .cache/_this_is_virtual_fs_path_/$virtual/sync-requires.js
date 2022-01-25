bf36bbbf610e039655cc43d89dad3167c3a9815a
TimMaina <timmaina64@gmail.com>
2022-01-25 21:01:20 +0300

    first commit

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,13 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\TIM\\Documents\\PERSONAL PROJECT FILES\\GATSBY\\blog\\timblog\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\TIM\\Documents\\PERSONAL PROJECT FILES\\GATSBY\\blog\\timblog\\src\\pages\\404.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\TIM\\Documents\\PERSONAL PROJECT FILES\\GATSBY\\blog\\timblog\\src\\pages\\index.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("C:\\Users\\TIM\\Documents\\PERSONAL PROJECT FILES\\GATSBY\\blog\\timblog\\src\\pages\\using-typescript.tsx")),
+  "component---src-templates-blog-post-js": preferDefault(require("C:\\Users\\TIM\\Documents\\PERSONAL PROJECT FILES\\GATSBY\\blog\\timblog\\src\\templates\\blog-post.js"))
+}
+
